fad9495c2aee6179d8185216bc7ee4989c24fc65
Vasiliyzhd <zhdanov.vasia@gmail.com>
2022-06-23 22:14:03 +0300

    nice view for code

--- test_registration2.py
+++ test_registration2.py
@@ -1,34 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import unittest
+import time
+
+
+def reg(link):
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']").send_keys('Ivan')
+    browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']").send_keys('Ivanov')
+    browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']").send_keys('ivan@mail.com')
+    browser.find_element(By.CSS_SELECTOR, "[type='submit']")
+
+    return browser.find_element(By.TAG_NAME, "h1").text
 
 
 class Test_registration(unittest.TestCase):
+
     def test_reg1(self):
-        browser = webdriver.Chrome()
-        browser.get("http://suninjuly.github.io/registration1.html")
-        input_fname = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
-        input_fname.send_keys('Ivan')
-        input_lname = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
-        input_lname.send_keys('Ivanov')
-        input_email = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
-        input_email.send_keys('ivan@mail.com')
-        button_submit = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
-        button_submit.click()
-        browser.quit()
+        self.assertEqual(reg("http://suninjuly.github.io/registration1.html"),
+                         "Registration", "Registration is failed")
 
     def test_reg2(self):
-        browser = webdriver.Chrome()
-        browser.get("http://suninjuly.github.io/registration2.html")
-        input_fname = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
-        input_fname.send_keys('Ivan')
-        input_lname = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
-        input_lname.send_keys('Ivanov')
-        input_email = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
-        input_email.send_keys('ivan@mail.com')
-        button_submit = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
-        button_submit.click()
-        browser.quit()
+        self.assertEqual(reg("http://suninjuly.github.io/registration2.html"),
+                         "Registration", "Registration is failed")
 
 
 if __name__ == "__main__":
