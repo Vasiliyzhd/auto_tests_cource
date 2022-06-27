190277dc9df41d8f5fb7d138ca45ce55a4b85619
Vasiliyzhd <zhdanov.vasia@gmail.com>
2022-06-27 11:16:15 +0300

    add xfailed mark

--- test_registration2.py
+++ test_registration2.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import unittest
@@ -6,25 +7,27 @@ import unittest
 def reg(link):
     browser = webdriver.Chrome()
     browser.get(link)
+    try:
+        browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']").send_keys('Ivan')
+        browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']").send_keys('Ivanov')
+        browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']").send_keys('ivan@mail.com')
+        browser.find_element(By.CSS_SELECTOR, "[type='submit']")
 
-    browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']").send_keys('Ivan')
-    browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']").send_keys('Ivanov')
-    browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']").send_keys('ivan@mail.com')
-    browser.find_element(By.CSS_SELECTOR, "[type='submit']")
-
-    return browser.find_element(By.TAG_NAME, "h1").text
+        return browser.find_element(By.TAG_NAME, "h1").text
+    finally:
+        browser.quit()
 
 
-class Test_registration(unittest.TestCase):
+class Test_registration:
 
     def test_reg1(self):
-        self.assertEqual(reg("http://suninjuly.github.io/registration1.html"),
-                         "Registration", "Registration is failed")
+        # self.assertEqual(reg("http://suninjuly.github.io/registration1.html"),
+        #                  "Registration", "Registration is failed")
+        assert reg("http://suninjuly.github.io/registration1.html") == "Registration", "Registration is failed"
 
+    @pytest.mark.xfail
     def test_reg2(self):
-        self.assertEqual(reg("http://suninjuly.github.io/registration2.html"),
-                         "Registration", "Registration is failed")
+        # self.assertEqual(reg("http://suninjuly.github.io/registration2.html"),
+        #                  "Registration", "Registration is failed")
+        assert reg("http://suninjuly.github.io/registration2.html") == "Registration", "Registration is failed"
 
-
-if __name__ == "__main__":
-    unittest.main()
